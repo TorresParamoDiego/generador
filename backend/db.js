22436b5f187c748007183bd0a7b6b46d9d130659
Diego Torres <torresparamodiegoalejandro@gmail.com>
2026-05-28 11:59:26 -0600

    Correcciones para deploy

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,15 @@
-
-//conexion principal con sequelize a postgres, 
-//se exporta para usar en otros archivos
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false  // ← requerido por Neon
+    }
   }
-);
+});
 
 export default sequelize;
