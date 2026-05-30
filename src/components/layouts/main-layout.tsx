1ef79d75a77be06a498248d5b4604bac23a156fd
Diego Torres <torresparamodiegoalejandro@gmail.com>
2026-05-29 21:33:56 -0600

    si

--- src/components/layouts/main-layout.tsx
+++ src/components/layouts/main-layout.tsx
@@ -7,9 +7,9 @@ export const MainLayout = ({children}:Props) => {
     <main className='py-8 px-4 bg-[url(assets/images/background-mobile.png)] bg-cover bg-bottom md:bg-[url(assets/images/background-tablet.png)] lg:bg-[url(assets/images/background-desktop.png)] relative overflow-hidden min-h-screen'>
 
       <img 
-        src="/assets/images/pattern-squiggly-line-top.svg" 
-        alt="pattern squiggly line top"
-        className='absolute top-4 right-0 h-[52px] md:h-[100px] lg:h-[200px]'
+        alt="pattern squiggly line top" 
+        className="absolute top-4 right-0 h-[52px] md:h-[100px] lg:h-[200px] pointer-events-none" 
+        src="/assets/images/pattern-squiggly-line-top.svg"
       />
 
       <img 
